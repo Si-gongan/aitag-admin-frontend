42618a2e25cd8560b7f0019103e1e1d057d4dcf2
sooki88 <sookhee9n@gmail.com>
2024-06-21 19:32:28 +0900

    Feat: fetcher와 getData 함수 구현

--- src/components/main/ListSection.tsx
+++ src/components/main/ListSection.tsx
@@ -1,4 +1,10 @@
-export default function ListSection() {
+import { DataType } from '@/services/main/scheme';
+
+interface ListSectionProps {
+  data: DataType;
+}
+
+export default function ListSection({ data }: ListSectionProps) {
   return (
     <section className="flex flex-wrap gap-y-37 justify-between">
       <div className="flex items-center justify-center w-224 h-245 rounded-lg bg-gray-300 text-24 text-white">
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
 export const SORT_BUTTONS = [{ text: '전체' }, { text: '진행중' }, { text: '완료' }];
+
+export const MAIN_DATA_LIMIT = '14';
